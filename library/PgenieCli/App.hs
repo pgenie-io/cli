d55a9a5037003cc1861375aa3d53d6edaac87b9a
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-09 01:31:34 +0300

    Simplify

--- library/PgenieCli/App.hs
+++ library/PgenieCli/App.hs
@@ -3,84 +3,55 @@ module PgenieCli.App (main) where
 import qualified Data.Text.IO as TextIO
 import qualified Network.HTTP.Client as HttpClient
 import qualified Network.HTTP.Client.TLS as HttpClientTls
-import Optics
-import Optics.State.Operators
 import qualified PgenieCli.Config.Model as Config
 import qualified PgenieCli.Config.Parsing as Parsing
 import PgenieCli.Prelude
 import qualified PgenieService as Client
 
 main :: IO ()
-main = runProcess $ do
-  migrations <- readMigrations
-  queries <- readQueries
-  response <- generate migrations queries
-  handleResponse response
-
-runProcess :: Process a -> IO a
-runProcess process = do
+main = do
   config <- Parsing.fileInDir mempty
   clientConfig <- Client.newConfig
   manager <- HttpClientTls.newTlsManager
-  res <- runExceptT $ runReaderT process (config, manager, clientConfig)
-  either (die . printBroadAs) return res
+  migrations <- readMigrations (#migrationsDir config)
+  queries <- readQueries (#queriesDir config)
+  generate manager clientConfig (#outputDir config) (#org config) (#name config) migrations queries
 
--- * Helpers
-
-type Process =
-  ReaderT Env (ExceptT Error IO)
-
-type Env =
-  (Config.Project, HttpClient.Manager, Client.PgenieServiceConfig)
-
-data Error
-  = MimeError !Client.MimeError
-
-instance BroadPrinting Error where
-  toBroadBuilder =
-    error "TODO"
-
-runClientRequest ::
-  (Client.Produces req accept, Client.MimeUnrender accept res, Client.MimeType contentType) =>
-  Client.PgenieServiceRequest req contentType res accept ->
-  Process res
-runClientRequest req =
-  ReaderT $ \(_, manager, clientConfig) ->
-    ExceptT $ fmap (first MimeError) $ Client.dispatchMime' manager clientConfig req
-
-readMigrations :: Process [(Text, Text)]
+readMigrations :: Path -> IO [(Text, Text)]
 readMigrations =
   error "TODO"
 
-readQueries :: Process [(Text, Text)]
+readQueries :: Path -> IO [(Text, Text)]
 readQueries =
   error "TODO"
 
-generate :: [(Text, Text)] -> [(Text, Text)] -> Process [Client.CodegenPost200ResponseInner]
-generate migrations queries = do
-  org <- gview (_1 % #org)
-  name <- gview (_1 % #name)
-  runClientRequest $ Client.codegenPost (req org name)
-  where
-    req org name =
-      Client.CodegenPostRequest org' name' migrations' queries'
-      where
-        org' =
-          fromNameIn #spinal org
-        name' =
-          fromNameIn #spinal name
-        migrations' =
-          migrations <&> uncurry Client.CodegenPostRequestMigrationsInner
-        queries' =
-          queries <&> uncurry Client.CodegenPostRequestQueriesInner
-
-handleResponse :: [Client.CodegenPost200ResponseInner] -> Process ()
-handleResponse results = do
-  outputDir <- gview (_1 % #outputDir)
+generate ::
+  HttpClient.Manager ->
+  Client.PgenieServiceConfig ->
+  Path ->
+  Name ->
+  Name ->
+  [(Text, Text)] ->
+  [(Text, Text)] ->
+  IO ()
+generate manager clientConfig outputDir org name migrations queries = do
+  res <-
+    Client.dispatchMime' manager clientConfig $
+      Client.codegenPost $
+        Client.CodegenPostRequest
+          (fromNameIn #spinal org)
+          (fromNameIn #spinal name)
+          (migrations <&> uncurry Client.CodegenPostRequestMigrationsInner)
+          (queries <&> uncurry Client.CodegenPostRequestQueriesInner)
+  results <- case res of
+    Left err -> die (to (show err))
+    Right res -> return res
   forM_ results $ \(Client.CodegenPost200ResponseInner pathText contents) -> do
     path <- parsePath pathText
     liftIO $ TextIO.writeFile (printCompactAs (outputDir <> path)) contents
 
-parsePath :: Text -> Process Path
-parsePath =
-  error "TODO"
+parsePath :: Text -> IO Path
+parsePath text =
+  case parseTextLeniently text of
+    Right path -> return path
+    Left err -> die (to err)
