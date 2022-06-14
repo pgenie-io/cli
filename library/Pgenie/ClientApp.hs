e313677cf9bccc41873398e942cbcb87b6876743
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-14 21:50:40 +0300

    Add tracing

--- library/Pgenie/ClientApp.hs
+++ library/Pgenie/ClientApp.hs
@@ -1,6 +1,8 @@
 module Pgenie.ClientApp (main) where
 
+import qualified Coalmine.BaseExtras.List as List
 import qualified Coalmine.EvenSimplerPaths as Path
+import qualified Coalmine.MultilineTextBuilder as Mb
 import qualified Data.Text.IO as TextIO
 import qualified Pgenie.Client as Client
 import Pgenie.ClientApp.Prelude
@@ -41,12 +43,21 @@ generate ::
   [(Path, Text)] ->
   IO ()
 generate secure host port config migrations queries = do
+  traceM [i|Requesting|]
   res <- Client.operateGlobally op secure host port
   results <- case res of
     Left err -> die (to err)
     Right res -> case res of
       Left err -> die (to err)
       Right res -> return res
+  traceM $
+    let results' =
+          results
+            & List.mapIntercalate (mappend "- " . Mb.indent 2 . toBroadBuilder) "\n"
+     in [i|
+          Results:
+            $results'
+        |]
   forM_ results $ \(path, contents) -> do
     liftIO $ TextIO.writeFile (printCompactAs (#outputDir config <> path)) contents
   where
