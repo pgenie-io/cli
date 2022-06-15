418f9c9f7f15d282ce9013a3ba11c738ff2a49e4
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-15 22:47:16 +0300

    Update deps and ditch prelude

--- library/Pgenie/ClientApp/Prelude.hs
+++
@@ -1,8 +0,0 @@
-module Pgenie.ClientApp.Prelude
-  ( module Exports,
-  )
-where
-
-import Coalmine.Inter as Exports
-import Coalmine.Name as Exports (FromNameInLowerCamelCase (..), FromNameInSpinalCase (..), FromNameInUpperCamelCase (..), Name)
-import Coalmine.Prelude as Exports hiding (Op)
