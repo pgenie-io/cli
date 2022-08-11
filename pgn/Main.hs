5d8def0c05541b2de323fdd585b35ae5e880f6c5
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-12 02:26:30 +0300

    Reduce the exe

--- pgn/Main.hs
+++ pgn/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+import Coalmine.Prelude
+import qualified Pgenie.Api.Client as Client
+import qualified Pgenie.Api.Protocol as Protocol
 import qualified Pgenie.Cli as App
 
-main = App.main
+main =
+  let op = do
+        Client.process (Protocol.Version 1 0 0) 1 config mempty mempty
+      config =
+        [i|
+          space: my-space
+          name: music-catalogue
+          version: 1.0.0
+        |]
+   in traceShowM =<< Client.run op True "api.pgenie.io" Nothing
